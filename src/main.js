290eceb6d8a92845ed98ca3329da03e75efc4f7e
jamezzz123 <chinwokwuj@gmail.com>
2021-01-28 12:01:54 +0100

    refactor: :heavy_plus_sign: add highchart vue to the project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import './plugins/bootstrap-vue';
+import './plugins/highchart';
 import './scss/main.scss';
 import router from './router';
 import store from './store';
