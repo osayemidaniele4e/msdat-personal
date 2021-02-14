ac59419b1947b2fa915d544e54da854d5001c638
augustine <aibe64@yahoo.com>
2021-02-14 14:28:07 +0100

    feat(austin-about_page): implement about page

---
+++ src/modules/msdat-dashboard/components/pageFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer id="page-footer">
+    <div>Built with <b-icon-heart-fill/> by eHealth4everyone</div>
+    <div>
+      <p>46 Indicators, 16 Data sources</p>
+      <p>Last Updated 12 Mar, 2019</p>
+    </div>
+  </footer>
+</template>
