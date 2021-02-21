b1990d6de7ee91f06d195cfbdccad4639b414c71
Prince <chimeremezechukwudire@gmail.com>
2021-02-21 16:00:23 +0100

    feat/barchart component updated

---
+++ src/components/charts/bar-chart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <highcharts :options="chartOptions" ref="lineCharts"></highcharts>
+  </div>
+</template>
+
+<script>
+import { Chart } from 'highcharts-vue';
+import Highcharts from 'highcharts';
+import stockInit from 'highcharts/modules/stock';
+
+stockInit(Highcharts);
+
+export default {
+  props: {
+    chartOptions: {
+      type: Object,
+    },
+  },
+  components: {
+    highcharts: Chart,
+  },
+};
+</script>
