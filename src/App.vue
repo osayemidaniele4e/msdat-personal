8ccae9ecb5ce44b5900428c117035d27c835b44b
jamesate4e <james.chinwokwu@e4email.com>
2021-04-24 21:10:48 +0100

    refactor: move chart component to root component folder

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,12 @@
 <template>
-  <div id="app">
-
-    <div class="mt-5">
-    <bar/>
-    </div>
-
+  <div>
     <router-view />
   </div>
 </template>
 
 <script>
-import bar from './modules/msdat-dashboard/components/barchart/index.vue';
-
-export default {
-  components: {
-    bar,
-  },
-};
+export default {};
 </script>
 
 <style lang="scss">
-#app {
-  -webkit-font-smoothing: antialiased;
-}
-
-#nav {
-  a {
-    font-weight: 600;
-    font-size: 0.875rem;
-    color: white;
-    padding: 4px 10px;
-
-    &.router-link-exact-active {
-      color: white;
-      background: #154736 0% 0% no-repeat padding-box;
-      border-radius: 5px;
-    }
-    &:hover {
-      text-decoration: none;
-    }
-  }
-}
 </style>
