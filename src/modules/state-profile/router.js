946975cb0a53326f0d7f4b5d488fbe4496ed435c
Ai-Bee <iboroinyang01@gmail.com>
2021-08-29 12:44:46 +0100

    Removed coming soon page from views

--- src/modules/state-profile/router.js
+++ src/modules/state-profile/router.js
@@ -1,31 +1,18 @@
 export default [
+  // {
+  //   path: '/state-profile',
+  //   name: 'comming soon',
+  //   component: () => import('./views/comingSoon.vue'),
+  // },
   {
-    path: '/state-profile',
-    name: 'comming soon',
-    component: () => import('./views/comingSoon.vue'),
-  },
-  {
-    path: '/state-profile/intro',
+    path: '/state-profile/',
     name: 'intro',
     component: () => import('./views/intro.vue'),
   },
   {
-    path: '/state-profile/state/:state',
+    path: '/state-profile/:state',
     name: 'state-profile',
     component: () => import('./views/stateProfile.vue'),
     props: true,
   },
 ];
-
-// {
-//   path: '/state-profile/:state',
-//   name: 'state-profile',
-//   component: () => import('./views/intro.vue'),
-//   props: true,
-//   children: [{
-//     path: '/:state/',
-//     name: 'state',
-//     component: () => import('./views/stateProfile.vue'),
-//     props: true,
-//   }],
-// },
