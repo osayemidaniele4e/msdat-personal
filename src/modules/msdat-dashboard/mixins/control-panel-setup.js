8742e305db221c14a444cb2f9d7485d20a6c8e80
jamesate4e <james.chinwokwu@e4email.net>
2021-08-29 23:51:27 +0100

    refactor: check for avaliable years

--- src/modules/msdat-dashboard/mixins/control-panel-setup.js
+++ src/modules/msdat-dashboard/mixins/control-panel-setup.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 // import { uniq, sortBy, groupBy } from 'lodash';
 import { uniq, sortBy } from 'lodash';
+import { isDataYearly } from '../../../util/helper';
 import { groupIndicator } from './util';
 
 export default {
@@ -29,9 +30,12 @@ export default {
         'program_area',
       );
       this.defaultDataSourceDropdown = this.dlGetDashboardDataSource();
-      this.defaultLocationDropdown = this.dlGetLocation({ level: 3 });
-
+      this.defaultLocationDropdown = this.dlGetLocation({
+        level: 3,
+      });
+      const location = this.dlGetLocation(1); // get nigerian Location object
       // add nigeria to the top of the array
+      this.defaultLocationDropdown.unshift(location);
     },
     setDefaults() {
       this.defaultIndicator = this.dlGetIndicator(this.$store.state.MSDAT_STORE.default.indicator);
@@ -40,10 +44,30 @@ export default {
       );
       this.defaultLocation = this.dlGetLocation(this.$store.state.MSDAT_STORE.default.location);
     },
+    async setYearDropdown() {
+      const data = await this.dlQuery({
+        indicator: this.defaultIndicator.id,
+        datasource: this.defaultDataSource.id,
+        location: this.defaultLocation.id,
+      });
+      console.log(data);
+      const onlyYearData = data.filter((item) => {
+        if (isDataYearly(item.period)) {
+          return item.period;
+        }
+        return false;
+      });
+      const years = onlyYearData.map((item) => item.period);
+      const unqiueYears = uniq(years);
+      const sortedYears = unqiueYears.sort((a, b) => b - a);
+      this.defaultYearDropdown = sortedYears;
+      // console.log(sortedYears);
+    },
   },
   async created() {
     this.setDefaults();
     await this.setUpControlPanelDropDown();
+    await this.setYearDropdown();
     this.cpIsLoading = true;
   },
 };
