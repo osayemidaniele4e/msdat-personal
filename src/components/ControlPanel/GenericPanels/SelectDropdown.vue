49a308321c890ad64c6d63ff30af0d435b17353f
jamesate4e <james.chinwokwu@e4email.net>
2021-08-30 19:30:05 +0100

    refactor: refactor multiselect instance to select first item on preselect prop

--- src/components/ControlPanel/GenericPanels/SelectDropdown.vue
+++ src/components/ControlPanel/GenericPanels/SelectDropdown.vue
@@ -1,17 +1,19 @@
 <template>
-<multiselect
+  <multiselect
     v-model="selected"
     :options="options"
     searchable
     close-on-select
-    :allow-empty=allowEmpty
+    :allow-empty="allowEmpty"
     placeholder="Pick a value"
     v-bind="multiSelectProps"
-    >
-</multiselect>
+  >
+  </multiselect>
 </template>
 
 <script>
+import { has } from 'lodash';
+
 export default {
   data() {
     return {
@@ -31,18 +33,30 @@ export default {
   props: {
     options: {
       type: Array,
-      default: () => ([]),
+      default: () => [],
     },
-    value: {
-    },
+    value: {},
     multiSelectProps: {
       type: Object,
       default: () => ({}),
+    },
+  },
+  watch: {
+    options(newValue) {
+      if (this.multiSelectProps['preselect-first']) {
+        debugger;
+        if (has(this.multiSelectProps, 'group-values')) {
+          // eslint-disable-next-line prefer-destructuring
+          this.selected = newValue[0][this.multiSelectProps['group-values']][0];
+        } else {
+          // eslint-disable-next-line prefer-destructuring
+          this.selected = newValue[0];
+        }
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
