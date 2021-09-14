10ad39532d976884137c296fe993d6e738cb4188
davebenard <benard.zibiri@e4email.net>
2021-09-14 08:31:26 +0100

    feat(file): create a dropcard file

---
+++ src/modules/msdat-dashboard/views/about/components/DropCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row text-center shadow">
+      <div class="col-md-3 mb-3">Loremlorem</div>
+      <div class="col-md-3 mb-3">Loremlorem</div>
+      <div class="col-md-3 mb-3">Loremlorem</div>
+      <div class="col-md-3 mb-3">Loremlorem</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
--- src/modules/msdat-dashboard/views/about/layout/theHeader.vue
+++ src/modules/msdat-dashboard/views/about/layout/theHeader.vue
@@ -11,11 +11,7 @@
           lg="11"
           class="d-flex justify-content-between align-items-center border-left"
         >
-          <h2>
-            MSDAT PLATFORM <br /><small
-              >ANALYSIS OF KEY HEALTH INDICATORS</small
-            >
-          </h2>
+          <h2>MSDAT PLATFORM <br /><small>ANALYSIS OF KEY HEALTH INDICATORS</small></h2>
 
           <!-- <b-col cols md="6" lg="6"> -->
           <div
@@ -31,28 +27,17 @@
               <router-link exact-active-class="active" to="/" class="nav-link"
                 >Main Dashboard</router-link
               >
-              <router-link to="#" class="nav-link"
-                >COVID-19: Service Uptake</router-link
-              >
-              <router-link to="#" class="nav-link"
-                >Custom Dashboard</router-link
-              >
+              <router-link to="#" class="nav-link">COVID-19: Service Uptake</router-link>
+              <router-link to="#" class="nav-link">Custom Dashboard</router-link>
               <b-nav-item>
                 <b-dropdown text="Other Dashboards" class="border-0">
-                  <div
-                    class="drop-container"
-                    v-for="(item, index) in headerDropdown"
-                    :key="index"
-                  >
-                    <router-link class="links" :to="item.link">{{
-                      item.title
-                    }}</router-link>
+                  <div class="drop-container" v-for="(item, index) in headerDropdown" :key="index">
+                    <router-link class="links" :to="item.link">{{ item.title }}</router-link>
                   </div>
                 </b-dropdown>
               </b-nav-item>
               <router-link to="#" class="nav-link"
-                ><b-icon-person-fill></b-icon-person-fill
-                >&nbsp;Login/Register</router-link
+                ><b-icon-person-fill></b-icon-person-fill>&nbsp;Login/Register</router-link
               >
             </b-nav>
             <b-icon
@@ -77,21 +62,24 @@
         <b-col class="">
           <h4>About the MSDAT Dashboard</h4>
           <p>
-            This dashboard is developed and managed by the Department of Health
-            Planning Research and Statistics (DHPRS)
+            This dashboard is developed and managed by the Department of Health Planning Research
+            and Statistics (DHPRS)
           </p>
         </b-col>
       </b-row>
     </b-container>
+<DropCard />
   </header>
 </template>
 
 <script>
 import HeaderOption from '../components/HeaderOption.vue';
+import DropCard from '../components/DropCard.vue';
 
 export default {
   components: {
     HeaderOption,
+    DropCard,
   },
   data() {
     return {
