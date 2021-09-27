5e664eb5eaf0dfd883b2c3a15d483e917c5476fc
jamesate4e <james.chinwokwu@e4email.net>
2021-09-27 15:21:57 +0100

    refactor: add functionalities to the about page

---
+++ src/modules/msdat-dashboard/views/about/components/AboutPageIndicator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row d-flex justify-content-between align-items-center">
+    <div class="col-3" v-for="(indicator, index) in indicators" :key="index">
+      <label for="">{{ indicator.program_area }}</label>
+      <b-form-select
+        :key="index"
+        :options="indicator.indicators"
+        :value="selected"
+        value-field="id"
+        text-field="short_name"
+      ></b-form-select>
+    </div>
+  </div>
+</template>
+
+<script>
+import { groupIndicator } from '@/util/helper';
+
+export default {
+  data() {
+    return {
+      indicators: [],
+      selected: 1,
+    };
+  },
+  mounted() {
+    this.indicators = groupIndicator(this.dlIndicator, 'program_area');
+    console.log(this.indicators);
+  },
+};
+</script>
+<style scoped>
+</style>
