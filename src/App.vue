33bb2e5ba5a395079597d6760335cb4462cc808e
Syed Aakif <69403829+SyedAakif@users.noreply.github.com>
2021-09-29 12:42:29 +0500

    msdat

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <the-header />
-  <router-view></router-view>
-  <the-footer class="fixed-bottom" />
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.min.css";
-import TheHeader from "./components/layout/TheHeader.vue";
-import TheFooter from "./components/layout/TheFooter.vue";
+
 export default {
-  name: "App",
-  components: {
-    TheHeader,
-    TheFooter,
-  },
 };
 </script>
 
-<style>
-* {
-  box-sizing: border-box;
-  font-size: 13px;
-}
+<style lang="scss">
 
-html {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
-
-body {
-  margin: 0;
-}
 </style>
