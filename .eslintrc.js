993b530d1eba1f256aae471639d5229d257d44a9
jamesate4e <james.chinwokwu@e4email.net>
2021-10-31 15:51:55 +0100

    feat(typescript): add typescript to dashboard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
   },
+
   overrides: [
     {
       files: [
@@ -25,5 +25,11 @@ module.exports = {
         jest: true,
       },
     },
+  ],
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript',
   ],
 };
