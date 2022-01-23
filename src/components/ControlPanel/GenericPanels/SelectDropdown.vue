c366142b42479c67cd4a6afc584e27d646b243f7
Ai-Bee <iboroinyang01@gmail.com>
2022-01-23 17:45:08 +0100

    feat: :building_construction: Indicator Clustering for multiselect
    Implemented indicator clustering based on program area for multiselect, extensive testing still ongoing

--- src/components/ControlPanel/GenericPanels/SelectDropdown.vue
+++ src/components/ControlPanel/GenericPanels/SelectDropdown.vue
@@ -2,6 +2,7 @@
   <!-- Label to show when there is no available data as requested
   for by chiamaka on the 2-12-2021 during msdat meeting -->
   <multiselect
+    @open="initialCSS"
     v-model="selected"
     :options="options"
     searchable
@@ -11,7 +12,22 @@
     v-bind="multiSelectProps"
     selectLabel=""
     deselectLabel=""
-    ><span class="text-capitalize" slot="noOptions">{{ NoDataLabel }}s</span></multiselect
+    >
+    <span class="text-capitalize" slot="noOptions">{{ NoDataLabel }}s</span>
+
+    <template v-if="multiSelectProps['group-values']" slot="option" slot-scope="props">
+       <template v-if="props.option.$groupLabel">
+      <span class="topicHead"
+       :data-parent="props.option.$groupLabel">
+       {{props.option.$groupLabel}}</span>
+      </template>
+    <div v-if="!props.option.$groupLabel"
+     :data-child="props.option.program_area">
+     {{props.option.full_name}}
+    </div>
+
+    </template>
+    </multiselect
   >
 </template>
 
@@ -70,8 +86,67 @@ export default {
     },
     immediate: false,
   },
-  mounted() {},
+  methods: {
+    pickProgramArea(event) {
+      const { parent } = event.target.children[0].children[0].dataset;
+      const all = Array.from(event.target.parentNode.children);
+      all.forEach((element) => {
+        // eslint-disable-next-line prefer-destructuring
+        const child = element.children[0].children[0].dataset.child;
+        if (parent === child) {
+          element.classList.toggle('noShow');
+        }
+      });
+    },
+    // hideOptions() {
+    //   if (this.multiSelectProps['group-values']) {
+    //     const all = document.querySelectorAll('div.multiselect__content-wrapper > ul > li');
+    //     // eslint-disable-next-line no-plusplus
+    //     for (let i = 0; i <= all.length; i++) {
+    // eslint-disable-next-line max-len
+    //       if (Array.from(all[i].children[0].classList).indexOf('multiselect__option--disabled') === -1) {
+    //         all[i].classList.add('noShow');
+    //       } else {
+    //         all[i].removeEventListener('click', (e) => {
+    //           this.pickProgramArea(e);
+    //         });
+    //       }
+    //     }
+    //   }
+    // },
+    initialCSS() {
+      if (this.multiSelectProps['group-values']) {
+        const all = document.querySelectorAll('div.multiselect__content-wrapper > ul > li');
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i <= all.length; i++) {
+          if (Array.from(all[i].children[0].classList).indexOf('multiselect__option--disabled') === -1) {
+            all[i].classList.add('noShow');
+          } else {
+            console.log(all[i]);
+            all[i].addEventListener('click', (e) => {
+              this.pickProgramArea(e);
+            });
+          }
+        }
+      }
+    },
+  },
+
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.noShow{
+  display: none;
+}
+
+.showContent{
+  display: block;
+}
+
+li.multiselect__element{
+  border-bottom: 1px solid #0000;
+  transition: all 3.5 ease-in
+}
+
+</style>
