92004ee45773d4776df981ac8d21664bfd5c13ec
davebenard <benard.zibiri@e4email.net>
2022-02-24 10:36:38 +0100

    fix: :zap: fix the 1dc place

--- src/modules/msdat-dashboard/components/sections/indicator-overview/TheIndicatorDatasoureComparisonChart.vue
+++ src/modules/msdat-dashboard/components/sections/indicator-overview/TheIndicatorDatasoureComparisonChart.vue
@@ -268,6 +268,7 @@ export default {
         let seriesObject = {};
         if (mappedValueTypes.length > 0) {
           const valueType = this.dlGetValueTypes(queryArray[index].value_type);
+          console.log(datasource);
           seriesObject = this.createSeriesObject(
             valueType,
             datasource.datasource,
@@ -359,6 +360,7 @@ export default {
     },
   },
   mounted() {
+    console.log('hello =>', this.ChartOptions);
     // debugger;
     // console.trace(this.values);
   },
