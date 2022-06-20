b831ea536bc6dd0814f2b476f2d335c858b2ef4d
davebenard <benard.zibiri@e4email.net>
2022-06-20 13:02:19 +0100

    create the clear data after a certain period of time

--- src/modules/DataLayer/store/actions.js
+++ src/modules/DataLayer/store/actions.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import DB from '../config/dexie';
 
 export default {
@@ -9,5 +10,24 @@ export default {
     await DB.delete();
     console.log('Database deleted');
     Promise.resolve(true);
+  },
+  // clear all data in dexie database after 1week from value
+  async CLEAR_DB() {
+    // const lastDate = localStorage.getItem('dataTimestamp');
+    const lastDate = '2022-04-20T11:54:13.824Z';
+    if (lastDate) {
+      const lastDateMoment = moment(lastDate);
+      const now = moment();
+      const diff = now.diff(lastDateMoment, 'days');
+      console.log('diff', diff, '2');
+      if (diff > 7) {
+        console.log('diff', diff);
+        //     await DB.clear();
+        //     console.log('Database cleared');
+        //     localStorage.setItem('dataTimestamp', moment().format());
+        //     Promise.resolve(true);
+      }
+    }
+    Promise.resolve(false);
   },
 };
