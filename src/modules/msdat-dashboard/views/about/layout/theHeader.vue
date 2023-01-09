e541744b55a28b5b80f3c84c4cca2902a72eb32b
Samuel <adetona.samuel@e4email.net>
2023-01-09 13:32:11 +0000

    progress

--- src/modules/msdat-dashboard/views/about/layout/theHeader.vue
+++ src/modules/msdat-dashboard/views/about/layout/theHeader.vue
@@ -110,8 +110,8 @@
                   </div>
                 </b-dropdown>
               </b-nav-item> -->
-              <div v-b-toggle.sidebar-2 v-if="isAuthenticated === false">
-                <b-icon-person-fill></b-icon-person-fill>&nbsp;Login/Register
+              <div v-b-toggle.sidebar-2 v-if="isAuthenticated === false" class="auth ml-2">
+                <b-icon-person-circle></b-icon-person-circle>&nbsp;Login/Register
               </div>
               <div v-else @click="showCard = true">
               <div class="ml-2 profile">
@@ -814,7 +814,7 @@ header#the-header {
   }
 }
 </style>
-<style lang="scss">
+<style lang="scss" scoped>
 header#the-header {
   div.header-navs {
     button.btn-secondary {
@@ -952,5 +952,8 @@ header#the-header {
 }
 .profile{
   cursor: pointer;
+}
+.auth {
+  font-size: 12px;
 }
 </style>
