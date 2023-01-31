160cf4d39160bbd1af537436eb43e3cd7134d180
traj3ctory <davidbenard.bd@gmail.com>
2023-01-31 16:31:33 +0100

    feat: create the service folder

--- src/modules/data-entry/services/index.ts
+++ src/modules/data-entry/services/index.ts
@@ -1,40 +1,46 @@
 import instance from '@/config/axios';
+import { dataSourceI } from './types';
 
-type selectType = { [key: string]: string | number };
-
-const getDataSources = async (id?: number) => {
-  const urlSource = `datasources/${id || '?size=1000'}`;
-  try {
-    const { data } = await instance.get(urlSource);
-    if (id) {
-      return data;
+class DataEntryService {
+  /**
+   * @function getDataSources
+   * @param {Number} id?
+   * @returns {Array}
+   */
+  getDataSources = async (id?: number): Promise<dataSourceI[] | dataSourceI> => {
+    const urlSource = `datasources/${id || '?size=1000'}`;
+    try {
+      const { data } = await instance.get(urlSource);
+      if (id) {
+        return data;
+      }
+      const dataArray = await data.results.map((el: dataSourceI) => ({
+        id: el.id,
+        name: el.datasource,
+      }));
+      return dataArray;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-    const dataArray = await data.results.map((el: selectType) => ({
-      id: el.id,
-      name: el.datasource,
-    }));
-    return dataArray;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+  };
 
-const getPeriodsByDs = async (id: number) => {
-  try {
-    const response = await getDataSources(id);
-    console.log(response.year_available, 'hello');
-    // const { data } = await instance.get(urlSource);
-    // const dataArray = await data.results.map((el: selectType) => ({
-    //   id: el.id,
-    //   name: el.datasource,
-    // }));
-    // console.log(dataArray);
-    // return dataArray;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+  getPeriodsByDs = async (id: number) => {
+    try {
+      const response = await this.getDataSources(id);
+      console.log(response, 'hello');
+      // const { data } = await instance.get(urlSource);
+      // const dataArray = await data.results.map((el: selectType) => ({
+      //   id: el.id,
+      //   name: el.datasource,
+      // }));
+      // console.log(dataArray);
+      // return dataArray;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+}
 
-export { getDataSources, getPeriodsByDs };
+export default new DataEntryService();
