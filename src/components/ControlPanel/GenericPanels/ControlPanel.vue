c68816ef3f0426e9e23a4e7191e436f04d006ed7
e4E SysAdmin <devops@e4email.net>
2023-07-27 08:35:09 +0000

    Revert "remove the yarn.lock file"
    This reverts merge request !313

--- src/components/ControlPanel/GenericPanels/ControlPanel.vue
+++ src/components/ControlPanel/GenericPanels/ControlPanel.vue
@@ -254,7 +254,7 @@ export default {
     checkNHMISDHIS2() {
       this.setup.forEach((item) => {
         if (item.key === 'datasource') {
-          const datasourceArr = item?.options.map((el) => el.id);
+          const datasourceArr = item?.options?.map((el) => el.id);
           if (datasourceArr?.includes(6)) {
             this.hasNHMIS = true;
           } else {
@@ -315,11 +315,12 @@ export default {
     const date = new Date();
     const getYear = date.getFullYear + 1;
     // pick one of the available years as the default years as opposed to the static 2016 year
-    const defaultYears = this.setup[3]?.options;
+    console.log('setup', this.setup);
+    const defaultYears = this.setup[3].options;
     // console.log(defaultYears);
     // console.log(defaultYears, 'defaultyears');
     const newArr = [];
-    defaultYears && defaultYears.map((el) => {
+    defaultYears.map((el) => {
       if (el < getYear) {
         newArr.push(el);
         this.defaultYearDropdown = newArr;
