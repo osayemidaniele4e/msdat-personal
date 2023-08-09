f3bc29cadb46de4279b3f3607fe74b3c8b5dcc6e
TolulopeJoshua <babtol235@gmail.com>
2023-08-09 07:56:04 +0100

    unit test: dragableList

---
+++ tests/unit/custom-dashboard/dragable-list.spec.js
@@ -0,0 +1,34 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import DragableList from '@/modules/custom-dashboard/components/Custom-dashboard-sections/Dragable-List.vue';
+
+import sections from './test-data/sections';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe('Getters.vue', () => {
+  let getters;
+  let store;
+
+  beforeEach(() => {
+    getters = {
+      arrangedSections: () => sections,
+    };
+
+    store = new Vuex.Store({
+      getters,
+    });
+  });
+
+  it('is a Vue instance', () => {
+    const wrapper = shallowMount(DragableList, { store, localVue });
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('renders correctly given the store data', () => {
+    const wrapper = shallowMount(DragableList, { store, localVue });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
---
+++ tests/unit/custom-dashboard/test-data/sections.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 0,
+    name: 'Indicator Overview',
+    active: false,
+    isShow: true,
+  },
+  {
+    id: 1,
+    name: 'Zonal analysis',
+    active: false,
+    isShow: true,
+  },
+  {
+    id: 2,
+    name: 'Indicator Comparison',
+    active: false,
+    isShow: true,
+  },
+  {
+    id: 3,
+    name: 'Dataset Comparison',
+    active: false,
+    isShow: true,
+  },
+  {
+    id: 4,
+    name: 'Multi-source Comparison',
+    active: false,
+    isShow: true,
+  },
+];
