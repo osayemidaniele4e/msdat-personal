b4592def1a5e9eea7c4b7b5668db1f05dab7fbd2
johnwav <102820657+johnwav@users.noreply.github.com>
2023-09-04 09:29:10 +0000

    generate component

---
+++ src/components/ControlPanel/components/generate.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="generate()">Generate</button>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Generate',
+  props: {
+  },
+  computed: {
+    ...mapGetters('MSDAT_STORE', ['getConfigObject', 'getSelectedConfig', 'getLoadingStatus']),
+  },
+  methods: {
+    generate() {
+      // use selected config to generate policy
+      console.log(this.getSelectedConfig);
+    },
+  },
+};
+</script>
+<style scoped>
+
+</style>
