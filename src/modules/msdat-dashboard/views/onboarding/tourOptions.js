cf41bf319763dd2fde3f93e0472b95f740f6eeeb
kodeman2 <holuwakoladey@gmail.com>
2023-12-21 08:38:51 +0100

    fixed ui issues on advanced analytics landing page

--- src/modules/msdat-dashboard/views/onboarding/tourOptions.js
+++ src/modules/msdat-dashboard/views/onboarding/tourOptions.js
@@ -4,31 +4,31 @@ const img2 = require('./assets/onboarding/Onboarding_Asset_2.svg');
 const steps = [
   {
     element: '.step-sections',
-    intro: `Select different types of analysis combinations <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/> `,
+    intro: `Select different types of analysis combinations <br/> <img src="${img}" height= "80px"/><img src="${img2}" height= "80px"/> `,
     title: 'Sections',
     position: 'bottom',
   },
   {
     element: '.step-controls ',
-    intro: `Controls all sections and variables from the control panel,  it changes per section. <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/>`,
+    intro: `Controls all sections and variables from the control panel,  it changes per section. <br/> <img src="${img}" height= "50px"/><img src="${img2}" height= "50px"/>`,
     title: 'Controls',
     position: 'bottom',
   },
   {
     element: '.step-indicator',
-    intro: `Click here to select your desired health indicator from this drop-down menu <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/> `,
+    intro: `Click here to select your desired health indicator from this drop-down menu <br/> <img src="${img}" height= "50px"/><img src="${img2}" height= "50px"/> `,
     title: 'Indicator',
     position: 'bottom',
   },
   {
     element: '.step-location',
-    intro: `Click here to select your desired location from the drop-down menu <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/> `,
+    intro: `Click here to select your desired location from the drop-down menu <br/> <img src="${img}" height= "50px"/><img src="${img2}" height= "50px"/> `,
     title: 'Location',
     position: 'bottom',
   },
   {
     element: '.step-datasource',
-    intro: `Click on any of these data sources to change the currently visualized datasource <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/>`,
+    intro: `Click on any of these data sources to change the currently visualized datasource <br/> <img src="${img}" height= "50px"/><img src="${img2}" height= "50px"/>`,
     title: 'Data source',
     position: 'bottom',
   },
@@ -48,7 +48,7 @@ const steps = [
     // element:
     //   ' div:nth-child(2) > div > div > div.card-body.work-sans.p-0 > div > div.col.p-2.bg-gray',
     element: '.confidenceRange_Intro',
-    intro: ` Toggle on the confidence range button to view data range from listed sources <br/> <img src="${img}" height= "40px"/><img src="${img2}" height= "40px"/> `,
+    intro: ` Toggle on the confidence range button to view data range from listed sources <br/> <img src="${img}" height= "50px"/><img src="${img2}" height= "50px"/> `,
     position: 'bottom',
     title: 'Trend Analysis',
   },
