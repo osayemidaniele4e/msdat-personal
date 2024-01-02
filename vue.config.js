4aedde0e8c1c0a81369bd96b18e75434d0751130
TolulopeJoshua <babtol235@gmail.com>
2024-01-02 11:13:36 +0100

    social share

--- vue.config.js
+++ vue.config.js
@@ -10,56 +10,33 @@ const plugins = [
     path.join(__dirname, 'dist'),
     // (REQUIRED) List of routes to prerender
     [
-      '/', '/dashboard', '/dashboard/Demographics',
-      '/dashboard/Health_Facility', '/dashboard/Health_Financing',
-      // '/dashboard/Health_Workforce', '/dashboard/Health_Service_Access', '/dashboard/Disease_Surveillance',
-      // '/dashboard/Health_Outcomes_and_Service_Coverage', '/dashboard/Advanced_Analytics',
+      '/',
     ],
     // (OPTIONAL) Compatible options from v2.
     {
-      // captureAfterDocumentEvent: 'custom-post-render-event',
-      // This is how you would trigger this example event:
-      // document.dispatchEvent(new Event('custom-post-render-event'))
-      // captureAfterElementExists: '#content',
       captureAfterTime: 10000,
-      //
-      // The function's context argument contains two properties:
-      //
-      // - html :: the resulting HTML after prerendering)
-      // - route :: the route currently being processed
-      //            e.g. "/", "/about", or "/contact")
-      //
-      // postProcessHtml(context) {
-      //   const titles = {
-      //     '/': 'Home',
-      //     '/about': 'Our Story',
-      //     '/contact': 'Contact Us',
-      //   };
-      //   return context.html.replace(
-      //     /<title>[^<]*<\/title>/i,
-      //     `<title>${titles[context.route]}</title>`,
-      //   );
-      // },
     },
   ),
   new PrerenderSPAPlugin(
-    // (REQUIRED) Absolute path to static root
     path.join(__dirname, 'dist'),
-    // (REQUIRED) List of routes to prerender
     [
-      // '/', '/dashboard', '/dashboard/Demographics',
-      // '/dashboard/Health_Facility', '/dashboard/Health_Financing',
-      // '/dashboard/Health_Workforce', '/dashboard/Health_Service_Access', '/dashboard/Disease_Surveillance',
-      '/dashboard/Health_Outcomes_and_Service_Coverage', '/dashboard/Advanced_Analytics',
+      '/dashboard', '/dashboard/Health_Outcomes_and_Service_Coverage', '/dashboard/Advanced_Analytics',
     ],
     {
       captureAfterTime: 10000,
     },
   ),
   new PrerenderSPAPlugin(
-    // (REQUIRED) Absolute path to static root
     path.join(__dirname, 'dist'),
-    // (REQUIRED) List of routes to prerender
+    [
+      '/dashboard/Health_Facility', '/dashboard/Health_Financing', '/dashboard/Demographics',
+    ],
+    {
+      captureAfterTime: 10000,
+    },
+  ),
+  new PrerenderSPAPlugin(
+    path.join(__dirname, 'dist'),
     [
       '/dashboard/Health_Workforce', '/dashboard/Health_Service_Access', '/dashboard/Disease_Surveillance',
     ],
