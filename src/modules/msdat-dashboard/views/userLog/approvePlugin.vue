3674813ac431b019b15d5221a3d124f1a6010837
Uyim Akpabio <akpabio98@gmail.com>
2024-01-09 10:32:08 +0100

    make changes to the admin page for approval of plugins

---
+++ src/modules/msdat-dashboard/views/userLog/approvePlugin.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <br>
+    <h3>Submitted Plugins</h3>
+    <div class="loader-screen" v-if="loading">
+      <b-spinner variant="success" label="Spinning"></b-spinner>
+    </div>
+    <div>
+      <b-row>
+        <template v-for="dashboard in userDashboards">
+          <b-col cols="4" :key="dashboard.id">
+            <b-card :title="dashboard.name" header-tag="header" footer-tag="footer">
+              <b-card-text>{{ dashboard.title }}</b-card-text>
+              <router-link target="_blank" :to="'/dashboard/' + dashboard.name">
+                <b-button variant="primary">Approve Pluigin</b-button>
+              </router-link>
+              <template #footer>
+                <em>{{ dashboard.visibility }}</em>
+              </template>
+            </b-card>
+          </b-col>
+        </template>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      loading: true,
+      userDashboards: {},
+    };
+  },
+
+  computed: {
+    ...mapGetters('AUTH_STORE', ['isAuthenticated', 'getUser', 'getDashboards']),
+  },
+
+  methods: {
+    ...mapActions('AUTH_STORE', ['SAVE_DASHBOARDS']),
+
+    filterArray(obj, arr) {
+      const userId = obj.id;
+      const filteredArr = arr.filter((item) => item.user === userId);
+      return filteredArr;
+    },
+  },
+
+  async mounted() {
+    this.loading = true;
+    await this.SAVE_DASHBOARDS();
+    this.userDashboards = this.filterArray(this.getUser, this.getDashboards.data);
+    this.loading = false;
+  },
+};
+</script>
+
+<style>
+.loader_screen {
+  width: 100vh;
+  height: 100vw;
+  justify-content: center;
+  align-items: center;
+}
+</style>
