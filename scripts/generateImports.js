2511e459f9aea20e127ab908988540793c4ca1bd
e4ewebdev <webdev@e4email.net>
2024-01-11 12:05:03 +0100

    updated script to generate code to import plugins in app.vue

--- scripts/generateImports.js
+++ scripts/generateImports.js
@@ -1,52 +1,9 @@
-// const fs = require('fs');
-// const path = require('path');
-
-// // Change these values to your actual component directory and desired output file
-// const basePath = path.join(__dirname, '../src/modules/plugins/');
-// const outputFile = path.join(__dirname, '../src/modules/plugins/index.js');
-
-// // Function to recursively find all '.vue' files in a directory
-// const findVueFiles = (dir) => {
-//   const files = fs.readdirSync(dir);
-//   let vueFiles = [];
-
-//   files.forEach((file) => {
-//     const filePath = path.join(dir, file);
-//     const stat = fs.statSync(filePath);
-
-//     if (stat.isDirectory()) {
-//       // If it's a directory, recursively search for '.vue' files
-//       vueFiles = vueFiles.concat(findVueFiles(filePath));
-//     } else if (file.endsWith('.vue')) {
-//       vueFiles.push(filePath);
-//     }
-//   });
-
-//   return vueFiles;
-// };
-
-// // Find all '.vue' files in subdirectories
-// const vueFiles = findVueFiles(basePath);
-
-// // Generate import statements
-// const imports = vueFiles.map((filePath) => {
-//   const componentName = path.basename(filePath, '.vue');
-//   return `import ${componentName} from './${path.relative(basePath, filePath)}';`;
-// });
-
-// const content = imports.join('\n');
-
-// // Write to the output file
-// fs.writeFileSync(outputFile, content, 'utf-8');
-
-// console.log('Import statements generated successfully!');
-
 const fs = require('fs');
 const path = require('path');
 
 // Change these values to your actual component directory and desired output file
 const basePath = path.join(__dirname, '../src/modules/plugins/');
-const outputFile = path.join(__dirname, '../src/modules/plugins/index.js');
+const outputFile = path.join(__dirname, '../src/App.vue');
 
 // Function to recursively find all '.js' files in a directory
 const findJSFiles = (dir) => {
@@ -71,35 +28,59 @@ const findJSFiles = (dir) => {
 // Find all '.js' files in subdirectories
 const jsFiles = findJSFiles(basePath);
 
-// Generate plugin install statements
-const pluginInstalls = jsFiles.map((filePath) => {
+// Generate plugin import statements and conditions
+const pluginImports = jsFiles.map((filePath) => {
   const componentName = path.basename(filePath, '.js');
-  return `import ${componentName} from './${path.relative(basePath, filePath)}';\nVue.use(${componentName});`;
+  return `import ${componentName} from './${path.relative(basePath, filePath)}';`;
 });
 
-// Additional lines of code
-const additionalCode = `
-if (!localStorage.getItem('contextPlugin')) {
-  localStorage.setItem('contextPlugin', 'false');
+const pluginInstalls = jsFiles.map((filePath) => {
+  const componentName = path.basename(filePath, '.js');
+  const subdirectoryName = path.basename(path.dirname(filePath));
+
+  return `
+if (!localStorage.getItem('${subdirectoryName}Plugin')) {
+  localStorage.setItem('${subdirectoryName}Plugin', 'false');
 }
 
-${pluginInstalls.join('\n')}
-
-if (localStorage.getItem('contextPlugin') === 'true') {
-  Vue.use(ContextPlugin);
-}
-if (!localStorage.getItem('indicatorPlugin')) {
-  localStorage.setItem('indicatorPlugin', 'false');
-}
-if (localStorage.getItem('indicatorPlugin') === 'true') {
-  Vue.use(IndicatorSearch);
+if (localStorage.getItem('${subdirectoryName}Plugin') === 'true') {
+  Vue.use(${componentName});
 }
 `;
+});
 
-// Combine import statements and additional code
-const content = additionalCode;
+// Additional code for App.vue
+const appVueCode = `
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+${pluginImports.join('\n')}
+
+export default {
+  mounted() {
+    ${pluginInstalls.join('\n')}
+  },
+  methods: {
+    ...mapGetters('MSDAT_STORE', ['getConfigObject']),
+  },
+};
+</script>
+
+<style lang="scss">
+.custom-swal-image {
+  margin: 0px !important; /* Adjust the margin as needed */
+  float: left; /* Align the image to the left of the text */
+}
+</style>
+`;
 
 // Write to the output file
-fs.writeFileSync(outputFile, content, 'utf-8');
+fs.writeFileSync(outputFile, appVueCode, 'utf-8');
 
-console.log('Plugin install statements and additional code generated successfully!');
+console.log('App.vue code generated successfully!');
