9d6b9568c02aa1c2cf41091dace726561d052b1c
e4esysadmin <devops@e4email.net>
2024-01-15 15:47:04 +0100

    Add 'reviewPlugin' plugin

---
+++ src/modules/plugins/reviewPlugin/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <ReviewPluginMain @setActiveComponent="setActiveComponent" v-if="activeComponent === 'main'" />
+      <ReviewPluginView @setActiveComponent="setActiveComponent" v-else />
+    </div>
+  </template>
+<script>
+import ReviewPluginView from './ReviewPlugin.vue';
+import ReviewPluginMain from './ReviewMain.vue';
+
+export default {
+  name: 'ContextPlugin',
+  components: {
+    ReviewPluginView,
+    ReviewPluginMain,
+  },
+  data() {
+    return {
+      activeComponent: 'main',
+    };
+  },
+  methods: {
+    setActiveComponent(component) {
+      this.activeComponent = component;
+    },
+  },
+};
+</script>
+  <style></style>
