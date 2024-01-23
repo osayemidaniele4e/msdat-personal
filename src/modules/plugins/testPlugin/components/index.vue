a82bc0cac9f4a4dfbea9142bc88911cda44d1e20
e4esysadmin <devops@e4email.net>
2024-01-23 11:29:08 +0000

    Add 'testPlugin' plugin

---
+++ src/modules/plugins/testPlugin/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <TestPluginMain @setActiveComponent="setActiveComponent" v-if="activeComponent === 'main'" />
+      <TestPluginView @setActiveComponent="setActiveComponent" v-else />
+    </div>
+  </template>
+<script>
+import TestPluginView from './TestPlugin.vue';
+import TestPluginMain from './TestMain.vue';
+
+export default {
+  name: 'ContextPlugin',
+  components: {
+    TestPluginView,
+    TestPluginMain,
+  },
+  data() {
+    return {
+      activeComponent: 'main',
+    };
+  },
+  methods: {
+    setActiveComponent(component) {
+      this.activeComponent = component;
+    },
+  },
+};
+</script>
+  <style></style>
