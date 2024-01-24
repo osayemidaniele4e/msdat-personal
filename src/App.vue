6d0702dd686faf2d30e4c4551f6d59e48de0e640
kodeman2 <holuwakoladey@gmail.com>
2024-01-24 09:25:29 +0100

    unit test for main dashboard

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,21 @@ import Vue from 'vue';
 import { mapActions, mapGetters } from 'vuex';
 import contextPlugin from './modules/plugins/contextPlugin';
 import indicatorPlugin from './modules/plugins/indicatorPlugin';
+import reviewPlugin from './modules/plugins/reviewPlugin';
+import testonePlugin from './modules/plugins/testonePlugin';
+import testPlugin from './modules/plugins/testPlugin';
 
 export default {
   data() {
     return {
       pluginsImported: [], // Explicitly specify the type as an array of strings
-      // pluginsImported: [], // Explicitly specify the type as an array of strings
     };
   },
   async mounted() {
-    // eslint-disable-next-line camelcase, no-unused-vars
+    /* eslint-disable camelcase */
+    // eslint-disable-next-line no-unused-vars
     const plugins_imported = [];
+    /* eslint-enable camelcase */
 
     this.pluginsImported.push('contextPlugin');
     if (!localStorage.getItem('contextPlugin')) {
@@ -37,6 +41,33 @@ export default {
 
     if (localStorage.getItem('indicatorPlugin') === 'true') {
       Vue.use(indicatorPlugin);
+    }
+
+    this.pluginsImported.push('reviewPlugin');
+    if (!localStorage.getItem('reviewPlugin')) {
+      localStorage.setItem('reviewPlugin', 'false');
+    }
+
+    if (localStorage.getItem('reviewPlugin') === 'true') {
+      Vue.use(reviewPlugin);
+    }
+
+    this.pluginsImported.push('testonePlugin');
+    if (!localStorage.getItem('testonePlugin')) {
+      localStorage.setItem('testonePlugin', 'false');
+    }
+
+    if (localStorage.getItem('testonePlugin') === 'true') {
+      Vue.use(testonePlugin);
+    }
+
+    this.pluginsImported.push('testPlugin');
+    if (!localStorage.getItem('testPlugin')) {
+      localStorage.setItem('testPlugin', 'false');
+    }
+
+    if (localStorage.getItem('testPlugin') === 'true') {
+      Vue.use(testPlugin);
     }
 
     console.log('pluginsImported', this.pluginsImported);
