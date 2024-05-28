102748dffcfcf7fb51a31177b7b2903949d24f4d
Linus Henry Ekene <henry.linus@e4email.net>
2024-05-28 13:46:55 +0000

    wip

--- src/modules/msdat-dashboard/components/sections/indicator-overview/TheTable.vue
+++ src/modules/msdat-dashboard/components/sections/indicator-overview/TheTable.vue
@@ -18,18 +18,7 @@
                 @click="toggleShowShareModal()"
                 class="share-btn"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-share-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"
-                  />
-                </svg>
+               <img src="@/assets/img/code.svg" alt="">
               </div>
             </div>
           </div>
@@ -365,8 +354,8 @@ export default {
   cursor: pointer;
 }
 
-.share-btn svg {
-  width: 12px;
+.share-btn img {
+  width: 20px;
   margin-right: 2px;
 }
 
