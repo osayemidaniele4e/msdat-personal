01c28eacb684296e02d5e7965fd83c758d62df1f
TolulopeJoshua <babtol235@gmail.com>
2024-08-22 13:08:43 +0100

    descriptive analysis

--- src/modules/msdat-dashboard/components/sections/advanced/descriptive-section/DescriptiveSection.vue
+++ src/modules/msdat-dashboard/components/sections/advanced/descriptive-section/DescriptiveSection.vue
@@ -1,91 +1,73 @@
 <template>
-  <div>
-    <div v-if="showComing" class="show-coming">
-      <div class="coming-soon-text">This section is coming soon....</div>
-      <img src="../../../../../../assets/svg/chart2.svg" alt="" class="svg1">
-    </div>
-     <!-- creating seaperate tabs for tableeu embeds
-     for each sub dashboard asides 'Health Outcomes' -->
-    <div v-if="title === 'Demographics'">
-      <div class="heading">
-        <!-- <div class="main-text">Demographics in Nigeria</div> -->
-        <!-- <div class="sub-text"> Source: NHA 2017 </div> -->
+  <div class="parent">
+    <div class="loader" v-if="isLoading">
+      <!-- <the-loader /> -->
+      <div class="spinner-border spinner-height text-success mx-3" role="status">
+        <span class="sr-only">Loading...</span>
       </div>
-      <b-embed type="iframe" aspect="21by9" :src="url" :width="width" :height="height"></b-embed>
     </div>
-    <div v-if="title === 'Health_Workforce'">
-      <div class="heading">
-        <!-- <div class="main-text">Health Workforce in Nigeria</div> -->
-        <!-- <div class="sub-text"> Source: NHA 2017 </div> -->
-      </div>
-      <b-embed type="iframe" aspect="21by9" :src="url2" :width="width" :height="height"></b-embed>
-    </div>
-    <div v-if="title === 'Health_Financing'">
-      <div class="heading">
-        <!-- <div class="main-text">Health Finance in Nigeria</div>
-        <div class="sub-text">Source: NHA 2017</div> -->
-      </div>
-      <b-embed type="iframe" aspect="21by9" :src="url3" :width="width" :height="height"></b-embed>
-    </div>
+    <b-embed
+      type="iframe"
+      :src="url"
+      :width="width"
+      :height="height"
+    ></b-embed>
   </div>
 </template>
+
 <script>
-import { mapMutations } from 'vuex';
-// import config from './config/dashboard_config';
+// import TheLoader from '@/modules/custom-dashboard/components/Loading/TheLoader.vue';
+
 export default {
-  name: 'DynamicDashboard',
+  name: 'DescriptiveAnalysis',
+  components: {
+    // TheLoader
+  },
   data() {
     return {
-      title: '',
-      //   dashboardConfig: config,
-      //   configObject: {}, // This should be an Object initially
-      // url: 'https://public.tableau.com/views/UpdatedDemographic1/Story?:language=en-US&:display_count=n&:origin=viz_share_link:showVizHome=no&:embed=true',
-      url: 'https://public.tableau.com/views/UpdatedDemographicDashboardVersion2_16472527584120/Dashboard3?:language=en-GB&:display_count=n&:origin=viz_share_link:showVizHome=no&:embed=true',
-      url2: 'https://public.tableau.com/views/HealthWorkforceworkingdocument_16472664254450/Dashboard1?:language=en-GB&:display_count=n&:origin=viz_share_link:showVizHome=no&:embed=true',
-      url3: 'https://public.tableau.com/views/Financedashboard_16472462810160/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link:showVizHome=no&:embed=true',
+      isLoading: true,
+      url: 'https://public.tableau.com/views/Descriptiveanalytics_17060149555940/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link:showVizHome=no&:embed=true',
       width: '100%',
-      height: '400',
-      showComing: true,
+      height: 'auto',
     };
   },
   methods: {
-    ...mapMutations('MSDAT_STORE', ['ADD_CONTROL_PANEL', 'CLEAR_CONTROL_PANEL']),
+    isLoaded() {
+      const canvas = document.querySelector('.embed-responsive-item');
+      const content = canvas.contentDocument || canvas.contentWindow.document;
+      const intervalId = setInterval(() => {
+        if (content.readyState === 'complete') {
+          this.isLoading = false;
+          clearInterval(intervalId);
+        } else {
+          console.error('canvas not found', canvas);
+        }
+      }, 1000);
+    },
   },
-  created() {
-    this.title = this.$route.params.name;
-    if (this.title === 'Demographics' || this.title === 'Health_Workforce' || this.title === 'Health_Financing') {
-      this.showComing = false;
-    }
+  mounted() {
+    this.isLoaded();
   },
 };
 </script>
+
 <style scoped>
-.heading {
-  margin: 10px;
-}
-.main-text {
-  font-size: 25px;
-  text-align: center;
-  color: black;
-}
-.sub-text {
-  color: black;
-  font-size: 15px;
-  text-align: left;
-  font-weight: 100;
-}
-.show-coming{
+.parent {
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 }
-.svg1{
-  width: 400px;
+.loader-img {
+  height: 300px;
+  width: 300px;
 }
-.coming-soon-text{
-  font-size: 15px;
-  font-weight: 500;
-  margin: 30px;
+
+.spinner-height {
+  height: 10rem;
+  width: 10rem;
+  margin: 3rem 0;
 }
 </style>
