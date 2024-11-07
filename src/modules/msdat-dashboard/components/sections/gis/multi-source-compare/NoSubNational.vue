feab7c3c29911af77a5f83e36487a0623cccd1c6
Linus Henry Ekene <henry.linus@e4email.net>
2024-11-07 10:50:36 +0000

    resolved feedback on gis mapping

---
+++ src/modules/msdat-dashboard/components/sections/gis/multi-source-compare/NoSubNational.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="py-5 custom-height">
+    <div class="px-4  w-100 d-flex justify-content-between align-items-center text-center ">
+      <span class="text-center text-item">Selected Indicator has only National Data</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.text-item {
+    font-size: 14px;
+    margin-left: 10px
+}
+.custom-height {
+    height: 400px !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
