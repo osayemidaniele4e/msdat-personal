c3f7c89e43554d41b211dfacce6cdab1c3ce987d
Linus Henry Ekene <henry.linus@e4email.net>
2024-11-07 12:32:10 +0000

    little fixes

--- src/modules/msdat-dashboard/components/sections/gis/multi-source-compare/NoSubNational.vue
+++ src/modules/msdat-dashboard/components/sections/gis/multi-source-compare/NoSubNational.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-5 custom-height">
-    <div class="px-4  w-100 d-flex justify-content-between align-items-center text-center ">
-      <span class="text-center text-item">Selected Indicator has only National Data</span>
+    <div class="w-100 d-flex text-group">
+      <span class="text-center text-item">Selected Indicator has only National Data </span>
     </div>
   </div>
 </template>
@@ -12,13 +12,18 @@ export default {};
 
 <style scoped>
 .text-item {
-    font-size: 14px;
-    margin-left: 10px
+  font-size: 14px;
+  margin-left: 10px;
+  font-weight: 700;
 }
 .custom-height {
-    height: 400px !important;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 400px !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.text-group {
+  display: flex;
+  justify-content: center;
 }
 </style>
