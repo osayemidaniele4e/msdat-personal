385db858acbd9e0177b0751e412db6bd8ae0121b
kodeman2 <holuwakoladey@gmail.com>
2024-11-08 08:27:49 +0100

    numbers showing on zonal section bug

--- src/modules/msdat-dashboard/components/sections/zonal-analysis/BaseZonalSectionComponent.vue
+++ src/modules/msdat-dashboard/components/sections/zonal-analysis/BaseZonalSectionComponent.vue
@@ -46,7 +46,8 @@ export default {
           id: 6,
           color: '#e1e164',
         },
-        { id: 7, color: '#7d8ade' },
+        // this code introduces the numbers showing in the zonal barchart bug - it has been reverted a couple of times.
+        // { id: 7, color: '#7d8ade' },
       ],
     };
   },
