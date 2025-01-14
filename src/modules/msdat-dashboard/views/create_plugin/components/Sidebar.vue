5242e26cd3596f74141e2477cc6b8a38c9573ec7
e4ewebdev <webdev@e4email.net>
2025-01-14 14:47:13 +0100

    ministers dashboard-reviewed

--- src/modules/msdat-dashboard/views/create_plugin/components/Sidebar.vue
+++ src/modules/msdat-dashboard/views/create_plugin/components/Sidebar.vue
@@ -78,9 +78,6 @@
           <router-link to="/dashboard/Health_Outcomes_and_Service_Coverage" target="_blank"  class="sub-item"
             >Health Outcomes and Service Coverage</router-link
           >
-          <router-link to="/ministers-dashboard" target="_blank"  class="sub-item"
-            >Minister of Health Dashboard</router-link
-          >
         </b-list-group>
       </div>
       <div class="col mb-3">
@@ -99,6 +96,9 @@
           <a href="https://ngf.fmohconnect.gov.ng/" target="_blank"
            class="sub-item"
             >Governors' Dashboard</a
+          >
+          <router-link to="/ministers-dashboard" target="_blank"  class="sub-item"
+            >SWAP Ministerial Dashboard</router-link
           >
           <!-- <router-link to="/coming-soon/advanced_analytics" -->
           <router-link to="/advanced_analytics"
