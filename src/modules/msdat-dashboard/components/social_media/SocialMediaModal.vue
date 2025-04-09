e77b8c22f1f11b8345ae02ed41ad49eef82cff04
Linus Henry Ekene <henry.linus@e4email.net>
2025-04-09 13:32:20 +0000

    fixed share dashbaord

--- src/modules/msdat-dashboard/components/social_media/SocialMediaModal.vue
+++ src/modules/msdat-dashboard/components/social_media/SocialMediaModal.vue
@@ -1,3 +1,5 @@
+<!-- eslint-disable camelcase -->
+<!-- eslint-disable camelcase -->
 <template>
   <div class="row">
     <div class="border-bottom col-12 pb-3">
@@ -67,7 +69,7 @@
       </div>
     </div>
     <div class="border-bottom col-12 pt-3 pb-3">
-      <h6 class="font-weight-bold work-sans">Share Specific Link</h6>
+      <h6 class="font-weight-bold work-sans">Share Specific LinkXX</h6>
       <div class="row no-gutters">
         <div class="col-md-9">
           <input
@@ -175,12 +177,59 @@ export default {
     ...mapGetters('MSDAT_STORE', ['getSelectedConfig']),
   },
   mounted() {
-    this.shareDesc = `Take a look at '${this.getSelectedConfig.indicator.full_name}' on the Multi-Source Data and Triangulation (MSDAT) platform`;
+    const advancedUrl2 = localStorage.getItem('advanced_url');
+    // this.shareDesc = `Take a look at '${this.getSelectedConfig.indicator.full_name}' on the Multi-Source Data and Triangulation (MSDAT) platform`;
+    const { name } = this.$route.params;
+    console.log(this.$route, '@@@');
+    console.log(advancedUrl2, '@@@ 2');
+    console.log(name, '@@@ 3');
 
-    const params = new URLSearchParams(window.location.search);
-    params.set('section', this.$store.state.MSDAT_STORE.selectedSection);
-    this.shareLink = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
-    window.history.pushState({}, '', this.shareLink);
+    if (name === 'Advanced_Analytics') {
+      const advancedUrl = localStorage.getItem('advanced_url');
+      this.shareLink = advancedUrl;
+    } else if (name === undefined) {
+      this.shareLink = `${window.location.origin}${window.location.pathname}`;
+      // logic for 'Other_Section'
+    } else {
+      const params = new URLSearchParams(window.location.search);
+      params.set('section', this.$store.state.MSDAT_STORE.selectedSection);
+      this.shareLink = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+      window.history.pushState({}, '', this.shareLink);
+    }
+    // if (name === 'Advanced_Analytics') {
+    //   const advancedUrl = localStorage.getItem('advanced_url');
+    //   this.shareLink = advancedUrl;
+    // } else if (name === 'health-service-uptake') {
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}`;
+    //   // logic for 'Other_Section'
+    // } else if (name === 'health-facilities-profile') {
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}`;
+    //   // logic for 'Other_Section'
+    // } else if (name === 'Quarterly_Performance_Assessment_Dashboard') {
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}`;
+    //   // logic for 'Other_Section'
+    // } else {
+    //   const params = new URLSearchParams(window.location.search);
+    //   params.set('section', this.$store.state.MSDAT_STORE.selectedSection);
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+    //   window.history.pushState({}, '', this.shareLink);
+    // }
+
+    // if (name === 'Advanced_Analytics') {
+    //   const advancedUrl = localStorage.getItem('advanced_url');
+    //   this.shareLink = advancedUrl;
+    // } else if {
+    //   const params = new URLSearchParams(window.location.search);
+    //   params.set('section', this.$store.state.MSDAT_STORE.selectedSection);
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+    //   window.history.pushState({}, '', this.shareLink);
+    // }else {
+    //   const params = new URLSearchParams(window.location.search);
+    //   params.set('section', this.$store.state.MSDAT_STORE.selectedSection);
+    //   this.shareLink = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+    //   window.history.pushState({}, '', this.shareLink);
+    // }
+
     // Update the shareable link with the new parameter
   },
 
