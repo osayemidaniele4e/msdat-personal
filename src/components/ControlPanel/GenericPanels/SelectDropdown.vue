d4d9558c3fa763c39f4858a1218969dc13657066
Henry <henry.linus@e4email.net>
2025-12-08 10:38:16 +0100

    wip

--- src/components/ControlPanel/GenericPanels/SelectDropdown.vue
+++ src/components/ControlPanel/GenericPanels/SelectDropdown.vue
@@ -1,64 +1,64 @@
 <template>
   <div class="">
-  <!-- <pre>{{ groupLabelStates }}</pre> -->
+    <!-- <pre>{{ groupLabelStates }}</pre> -->
     <multiselect
-    :id="formattedID"
-    v-model="selected"
-    :options="options"
-    searchable
-    close-on-select
-    :allow-empty="allowEmpty"
-    :placeholder="placeholder"
-    v-bind="multiSelectProps"
-    selectLabel=""
-    data-visted="notVisited"
-    deselectLabel=""
-    autocomplete="off"
-    class="custom-placeholder"
-    @open="handleOpen"
-    @close="handleClose"
-    @search-change="handleSearchChange"
-  >
-    <span class="text-capitalize" slot="noOptions">{{ NoDataLabel }}</span>
+      :id="formattedID"
+      v-model="selected"
+      :options="options"
+      searchable
+      close-on-select
+      :allow-empty="allowEmpty"
+      :placeholder="placeholder"
+      v-bind="multiSelectProps"
+      selectLabel=""
+      data-visted="notVisited"
+      deselectLabel=""
+      autocomplete="off"
+      class="custom-placeholder"
+      @open="handleOpen"
+      @close="handleClose"
+      @search-change="handleSearchChange"
+    >
+      <span class="text-capitalize" slot="noOptions">{{ NoDataLabel }}</span>
 
-    <template v-if="multiSelectProps['group-values']" slot="option" slot-scope="props">
-      <template v-if="props.option.$groupLabel">
-        <span class="overflow-textg" :data-parent="props.option.$groupLabel">
-          {{ props.option.$groupLabel }}
-          <span
-            v-if="isCollapsibleActive"
-            class="newGrouplabel"
-            :class="{ 'open-caret': groupLabelStates[props.option.$groupLabel] }"
-            @click.stop="toggleGroupLabel(props.option.$groupLabel)"
-          >
-            {{
-              groupLabelStates[props.option.$groupLabel]
-                ? 'Click to collapse ▲'
-                : 'Click to expand ▼'
-            }}
+      <template v-if="multiSelectProps['group-values']" slot="option" slot-scope="props">
+        <template v-if="props.option.$groupLabel">
+          <span class="overflow-textg" :data-parent="props.option.$groupLabel">
+            {{ props.option.$groupLabel }}
+            <span
+              v-if="isCollapsibleActive"
+              class="newGrouplabel"
+              :class="{ 'open-caret': groupLabelStates[props.option.$groupLabel] }"
+              @click.stop="toggleGroupLabel(props.option.$groupLabel)"
+            >
+              {{
+                groupLabelStates[props.option.$groupLabel]
+                  ? 'Click to collapse ▲'
+                  : 'Click to expand ▼'
+              }}
+            </span>
           </span>
-        </span>
+        </template>
+        <template v-if="props.option.item">
+          <div
+            v-if="!props.option.$groupLabel"
+            class="overflow-text"
+            :data-child="modifyDataSourceChildLabel(props.option.item)"
+          >
+            {{ props.option.item }}
+          </div>
+        </template>
+        <template v-else-if="props.option.full_name">
+          <div
+            v-if="!props.option.$groupLabel"
+            class="overflow-text text-wrap"
+            :data-child="props.option.program_area"
+          >
+            {{ props.option.full_name }}
+          </div>
+        </template>
       </template>
-      <template v-if="props.option.item">
-        <div
-          v-if="!props.option.$groupLabel"
-          class="overflow-text"
-          :data-child="modifyDataSourceChildLabel(props.option.item)"
-        >
-          {{ props.option.item }}
-        </div>
-      </template>
-      <template v-else-if="props.option.full_name">
-        <div
-          v-if="!props.option.$groupLabel"
-          class="overflow-text text-wrap"
-          :data-child="props.option.program_area"
-        >
-          {{ props.option.full_name }}
-        </div>
-      </template>
-    </template>
-  </multiselect>
+    </multiselect>
   </div>
 </template>
 <script>
@@ -87,10 +87,10 @@ export default {
       },
       set(val) {
         if (
-          val
-          && typeof val === 'object'
-          && val.id !== undefined
-          && val.program_area !== undefined
+          val &&
+          typeof val === 'object' &&
+          val.id !== undefined &&
+          val.program_area !== undefined
         ) {
           this.selectedOption = val;
           // this.indicatorId = val.id;
@@ -107,10 +107,10 @@ export default {
           localStorage.setItem('indicatorSecondRelated', indicatorSecondRelated);
           this.SET_SELECTED_CONFIG(item);
         } else if (
-          val
-          && typeof val === 'object'
-          && val.id !== undefined
-          && val.methodology !== undefined
+          val &&
+          typeof val === 'object' &&
+          val.id !== undefined &&
+          val.methodology !== undefined
         ) {
           // this.saveDataSourceToStorage(val.id);
           const item = {
@@ -124,10 +124,10 @@ export default {
           };
           this.SET_ZONAL_DATASOURCE(item2);
         } else if (
-          val
-          && typeof val !== 'object'
-          && val.id === undefined
-          && val.created_at === undefined
+          val &&
+          typeof val !== 'object' &&
+          val.id === undefined &&
+          val.created_at === undefined
         ) {
           const item = {
             payload: val,
@@ -181,7 +181,7 @@ export default {
     options: {
       async handler(newValue) {
         // this.addQueryParamToUrl();
-        
+
         this.loading = true;
         if (this.options?.length > 0) {
           if (this.multiSelectProps['preselect-first']) {
@@ -192,7 +192,7 @@ export default {
               if (name === 'Demographics') {
                 this.selected = '';
                 const newArr = this.options.filter(
-                  (year) => parseInt(year, 10) < new Date().getFullYear() + 1,
+                  (year) => parseInt(year, 10) < new Date().getFullYear() + 1
                 );
 
                 this.selected = newArr[0];
@@ -211,7 +211,7 @@ export default {
                 const year = date.getFullYear() - 1;
                 this.selected = {};
                 const newArr = this.newValue.filter(
-                  (item) => parseInt(item, 10) < new Date().getFullYear() + 1,
+                  (item) => parseInt(item, 10) < new Date().getFullYear() + 1
                 );
 
                 this.selected = newArr[0] || year.toString();
@@ -233,6 +233,9 @@ export default {
            * if its not an array, make the object the default selected
            */
 
+          // console.log(newValue, '@newValue');
+          // console.log(this.multiSelectProps, '@newValue 1');
+
           if (this.multiSelectProps?.label === 'datasource') {
             if (Array.isArray(newValue) && newValue?.length > 0) {
               this.UPDATE_ALL_DATASOURCES(newValue);
@@ -248,12 +251,8 @@ export default {
             this.selected = {};
             this.selected = await newValue[0];
           }
-           if (this.multiSelectProps?.key === 'location') {
-            // console.log(newValue, '@newValue location');
-            
-            
+          if (this.multiSelectProps?.key === 'location') {
             if (Array.isArray(newValue) && newValue?.length > 0) {
-             
               const defaultSelected = newValue.find((item) => item?.id === this.selected?.id);
               if (defaultSelected?.id !== undefined) {
                 this.selected = {};
@@ -265,6 +264,16 @@ export default {
             }
             // this.selected = {};
             // this.selected = await newValue[0];
+          }
+          if (Array.isArray(newValue) && newValue?.length > 0 && newValue[0].level !== undefined) {
+            const defaultSelected = newValue.find((item) => item?.id === this.selected?.id);
+            if (defaultSelected?.id !== undefined) {
+              this.selected = {};
+              this.selected = defaultSelected;
+              return;
+            }
+            this.selected = {};
+            this.selected = await newValue[0];
           }
         }
         this.loading = false;
